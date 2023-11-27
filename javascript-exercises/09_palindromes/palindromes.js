6f14c157bc54012d03a33ebcd765041ec295321e
kanta8864 <kantadelft@gmail.com>
2023-11-27 02:36:13 +0100

    javascript exercises done

--- javascript-exercises/09_palindromes/palindromes.js
+++ javascript-exercises/09_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    // remember that \W/ is a non alphanumeric characters. 
+    // /g is for gloabal search. (not to stop after first occurance)
+    let str = string.toLowerCase().replace(/\W/g, '')
+    let charArray = str.split("");
+    let reverseCharArray = charArray.reverse();
+    let reversedString = reverseCharArray.join("");
+    return str === reversedString
 };
 
 // Do not edit below this line
--- javascript-exercises/12_findTheOldest/findTheOldest.js
+++ javascript-exercises/12_findTheOldest/findTheOldest.js
@@ -1,5 +1,20 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    let sorted = people.sort((a,b)=> {
+        let aAge = 0;
+        let bAge = 0;
+        if(!a.yearOfDeath) {
+            aAge = 2023 - a.yearOfBirth;
+        }else {
+            aAge = a.yearOfDeath - a.yearOfBirth;
+        }
+        if(!b.yearOfDeath) {
+            bAge = 2023 - b.yearOfBirth;
+        }else {
+            bAge = b.yearOfDeath - b.yearOfBirth;
+        }
+        return aAge > bAge ? -1 : 1;
+    });
+    return sorted[0];
 };
 
 // Do not edit below this line
